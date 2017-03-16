731514da22dbacbe2fd84635e21a6e2b7787420d
maggie jv <maggie jv>
2017-03-16 10:59:09 -0600

    ejercicios-html

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,12 @@
-i try it but no
+function converFarenheit(){
+var far =document.getElementById("farenheit").value;
+var resultado = (far-32)*5/9;
+document.getElementById("farenheit").value = resultado.toFixed(2);
+console.log(resultado.toFixed(2));
+}
+function converCelsius(){
+var cel =document.getElementById("celsius").value;
+var resultad = cel*5/9+32;
+document.getElementById("celsius").value = resultad.toFixed(2);
+console.log(resultad.toFixed(2));
+}
